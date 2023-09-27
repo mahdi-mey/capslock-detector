6710f27d7d6e77926239687a35b959ca92e99528
Mahdi <mahdimey17@gmail.com>
2023-09-27 11:28:34 +0330

    added capslock warning

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const inputElement = document.querySelector('#capslockDetector')
 const capsWarning = document.querySelector('.caps')
 const numWarning = document.querySelector('.num')
+
+inputElement.addEventListener('keyup', (e) => {
+    let isCapsActive = e.getModifierState('CapsLock')
+
+    if(isCapsActive){
+        capsWarning.style.display = 'block'
+    }
+    else{
+        capsWarning.style.display = 'none'
+    }
+    
+})
